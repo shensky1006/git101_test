cdd00b676164c3d3217e51fa92e958e520760cd0
沈世凱 <qq19981006@gmail.com>
2025-03-29 22:22:20 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+total 16
+drwxr-xr-x   9 sky  staff  288  3 29 22:16 .
+drwxr-x---+ 21 sky  staff  672  3 29 22:18 ..
+drwxr-xr-x  15 sky  staff  480  3 29 16:05 .git
+-rw-r--r--   1 sky  staff    5  3 28 13:45 .gitignore
+-rw-r--r--   1 sky  staff    0  3 28 13:57 NewFile
+-rw-r--r--   1 sky  staff   38  3 29 16:05 code.js
+-rw-r--r--   1 sky  staff    0  3 29 22:20 index.js
+-rw-r--r--   1 sky  staff    0  3 28 11:36 test
+-rw-r--r--   1 sky  staff    0  3 28 11:35 test.txt
